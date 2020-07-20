cc6b7431d6441558be9d871f98ec61ab68ebea73
John Pete <johnpete@Johns-MacBook-Pro.local>
2020-07-20 16:44:34 +0200

    initial commit

---
+++ serverbackup02.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'johnpete',
+        password: '',
+        database: 'smart-brain'
+    }
+});
+
+const app = express();
+
+app.use(cors())
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send(database.users);
+})
+
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) }) // dependecy injection of db knex and bcrypt
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0]);
+        })
+        .catch(err => res.status(400).json('unable to get entries'))
+})
+
+app.listen(3000, () => {
+    console.log('app is running on port 3000');
+})
